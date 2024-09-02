24c77f500069169f1c1d5312c94349089e543efc
Alisa <alisa.melissa@icloud.com>
2024-09-02 14:09:12 +0300

    Initial Commit

---
+++ BubbleButton/BubbleButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleButtonApp.swift
+//  BubbleButton
+//
+//  Created by Алиса Третьякова on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BubbleButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BubbleButton
+//
+//  Created by Алиса Третьякова on 02.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
